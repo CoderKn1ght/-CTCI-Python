6faa59899ca5a09f4ad227a6a65f9d0fb8002b34
Shashank Gaikaiwari <shashank.gaikaiwari@gmail.com>
2019-02-02 01:21:43 -0600

    Added Bit Manipulation Solution in q1

--- CH1_Arrays_and_Strings/q1_string_all_unique_characters.py
+++ CH1_Arrays_and_Strings/q1_string_all_unique_characters.py
@@ -3,8 +3,7 @@ import time
 # Assumptions made: input uses a ASCII (128) character set
 # Uses a boolean list. At most needs to go over 128 characters.
 
-
-def check_all_unique_boolean_list(string_to_be_tested):
+def is_unique_characters_boolean(string_to_be_tested):
     if len(string_to_be_tested) > 128:
         return False
     character_set_check = [False] * 128
@@ -15,10 +14,9 @@ def check_all_unique_boolean_list(string_to_be_tested):
         character_set_check[ascii_value] = True
     return True
 
-
 # Solution 2
 # Uses Dictionary
-def check_all_unique_dictionary(string_to_be_tested):
+def is_unique_characters_dictionary(string_to_be_tested):
     character_set = {}
     for char in string_to_be_tested:
         if char not in character_set:
@@ -28,20 +26,41 @@ def check_all_unique_dictionary(string_to_be_tested):
     return True
 
 
+# Solution 3
+# Uses Bit Manipulation
+
+def is_unique_characters_bit(string_to_be_tested):
+    checker = 0
+    for char in string_to_be_tested:
+        ascii_value = ord(char) - ord('a')
+        if (checker & (1 << ascii_value)) > 0:
+            return False
+        checker = checker | (1 << ascii_value)
+    return True
+
+
 start_time = time.perf_counter()
-print(check_all_unique_boolean_list('aucxizlxk'))
+print(is_unique_characters_boolean('aucxizlk'))
 end_time = time.perf_counter()
 time_taken = (end_time-start_time)
 print('Boolean : ' + "%.10f" % time_taken + " seconds")
 
 start_time = time.perf_counter()
-print(check_all_unique_dictionary('aucxizlxk'))
+print(is_unique_characters_dictionary('aucxizlk'))
 end_time = time.perf_counter()
 time_taken = (end_time-start_time)
 print('Dictionary : ' + "%.10f" % time_taken + " seconds")
 
+start_time = time.perf_counter()
+print(is_unique_characters_bit('asdfgh'))
+end_time = time.perf_counter()
+time_taken = (end_time-start_time)
+print('Bit : ' + "%.10f" % time_taken + " seconds")
+
 # Output Obtained
-# False
-# Boolean : 0.0000197465 seconds
-# False
+# True
+# Boolean : 0.0000204518 seconds
+# True
 # Dictionary : 0.0000063471 seconds
+# True
+# Bit : 0.0000084628 seconds
