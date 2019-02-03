7ae5926309349f604d0ed1a5de0f689957b7bcd4
Shashank Gaikaiwari <shashank.gaikaiwari@gmail.com>
2019-02-02 20:44:26 -0600

    Modified Iterate Method

--- CH2_Linked_Lists/Node.py
+++ CH2_Linked_Lists/Node.py
@@ -1,5 +1,10 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
+class Node(object):
+    def __init__(self, val):
+        self.val = val
         self.next = None
+
+    def iterate_and_print(self):
+        while self:
+            print(self.val)
+            self = self.next
 
---
+++ CH2_Linked_Lists/Traverse_List.py
@@ -0,0 +1,10 @@
+from CH2_Linked_Lists.Node import Node
+
+
+if __name__ == '__main__':
+    n1 = Node(5)
+    n2 = Node(7)
+    n3 = Node(3)
+    n1.next = n2
+    n2.next = n3
+    n1.iterate_and_print()
